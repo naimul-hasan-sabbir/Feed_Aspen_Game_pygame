2fd082096b163f11bf202b5073b035bdd7c0e0dd
naimul-hasan-sabbir <naimulislamsabbir2613@gmail.com>
2023-12-04 13:21:59 +0600

    first commit

---
+++ game.py
@@ -0,0 +1,44 @@
+import pygame
+
+#pygame setup
+pygame.init()
+screen = pygame.display.set_mode((800,500))
+pygame.display.set_caption("Naimul's Pygame Tutorial")
+clock = pygame.time.Clock()
+running = True
+
+dt = 0
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+while running:
+    #poll for events
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    #pick screen color
+    screen.fill("silver")
+    
+    #render our game
+    pygame.draw.circle(screen, '#033660', player_pos, 40)
+    
+    #move the circle
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_DOWN]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_LEFT]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_RIGHT]:
+        player_pos.x += 300 * dt
+    
+    #flip the display
+    pygame.display.flip()
+    
+    dt = clock.tick(60) / 1000
+    
+    
+    
+pygame.quit()
